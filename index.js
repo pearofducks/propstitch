307b99ca2dab0ff2f30e3beb06a89cfc49715486
Dave Honneffer <pearofducks@gmail.com>
2020-10-06 07:25:15 +0200

    Start yaml

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-const components = require('./dist/components')
+import * as testComponents from './dist/components'
+import { loadYAML } from './yaml'
 
-const getDefault = prop => {
+const notes = loadYAML()
+export const getDefault = prop => {
   if (prop === null) return
   return 'default' in prop && typeof prop.default === 'function' ? JSON.stringify(prop.default()) : JSON.stringify(prop.default)
 }
-const getTypes = prop => {
+export const getTypes = prop => {
   if (prop === null) return ['Any']
   if (Array.isArray(prop)) return prop.flatMap(getTypes)
   if (typeof prop === 'object') return getTypes(prop.type)
   return 'name' in prop ? [prop.name] : ['Unknown']
 }
-const getRequired = prop => {
+export const getRequired = prop => {
   if (prop === null) return false
   if (typeof prop === 'object') return !!prop.required
   return false
 }
-const result = Object.entries(components).reduce((acc, [name, comp]) => {
+export const getNote = (compName, propName) => notes[compName][propName]
+export const getMetadata = compName => ({
+  subtitle: notes[compName].$subtitle,
+  description: notes[compName].$description,
+  token: notes[compName].$token,
+  slots: notes[compName].$slots
+})
+export const buildProps = (components) => Object.entries(components).reduce((acc, [name, comp]) => {
   acc[name] = Object.entries(comp.props).reduce((_acc, [k, v]) => {
-    _acc[k] = { types: getTypes(v), required: getRequired(v), default: getDefault(v) }
+    _acc[k] = { types: getTypes(v), required: getRequired(v), default: getDefault(v), note: getNote(name, k) }
     return _acc
   }, {})
+  acc[name].metadata = getMetadata(name)
   return acc
 }, {})
+
+
+const result = buildProps(testComponents)
+console.log(notes)
 console.log(result)
--- yaml.js
+++ yaml.js
@@ -1,4 +1,4 @@
 import yaml from 'js-yaml'
 import fs from 'fs'
 
-export const load () => yaml.safeLoad(fs.readFileSync('./annotations.yaml', 'utf-8'))
+export const loadYAML = () => yaml.safeLoad(fs.readFileSync('./documentation/components.yaml', 'utf-8'))
